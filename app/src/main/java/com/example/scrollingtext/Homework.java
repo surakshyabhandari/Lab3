50fe2716b5f372f777f48a304b9688ce54193ba0
surakshyabhandari <51433451+surakshyabhandari@users.noreply.github.com>
2022-03-21 18:07:40 +0545

    homework

--- app/src/main/java/com/example/scrollingtext/Homework.java
+++ app/src/main/java/com/example/scrollingtext/Homework.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Homework extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_homework);
     }
 
     public void launchHomework(View view) {
-        Intent intent = new Intent(this, Homework.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-
     }
 }
